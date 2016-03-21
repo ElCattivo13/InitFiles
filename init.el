26467f50f702d142361cf5063fef3b7f85639f47
Stephan Boehme <boehme@tcs.inf.tu-dresden.de>
2016-03-21 14:40:29 +0000

    workgroups

--- init.el
+++ init.el
@@ -104,7 +104,8 @@
 ;; What to do on Emacs exit / workgroups-mode exit?                                      ;
 (setq wg-emacs-exit-save-behavior           'save)      ; Options: 'save 'ask nil        ;
 (setq wg-workgroups-mode-exit-save-behavior 'save)      ; Options: 'save 'ask nil        ;
-                                                                                         ;
+					;
+(add-hook 'workgroups-mode-hook 'wg-reload-session)
                                                                                          ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
